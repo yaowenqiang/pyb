140b0c122c953817fac4a2ab007d3aefb5b81316
yaowenqiang <yaowenqiang111@163.com>
2022-02-02 23:33:30 +0800

    add numeric demo

---
+++ numeric.py
@@ -0,0 +1,21 @@
+num = 3.14
+
+print(type(num))
+print(3 / 2)    # 1.5
+print(3 // 2)   # 1
+print(3 ** 2) 
+print(3 % 2) 
+print(abs(-1)) 
+print(round(-1.5)) 
+print(round(1.5)) 
+print(round(1.55,1)) 
+
+
+num_1 = '100'
+num_2 = '200'
+
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
