f091763ee1706d504e3261eaba856ecec3d6e76a
yaowenqiang <yaowenqiang111@163.com>
2022-02-07 23:28:24 +0800

    add loops and iteration demo

---
+++ loops_and_iterations.py
@@ -0,0 +1,19 @@
+nums = [1,2,3,4,5]
+for num in nums:
+	if num == 4:
+		print('Found!')
+		break
+		continue
+	print(num)
+
+
+
+for i in range(1, 10):
+	print(i)
+
+
+x = 0
+
+while x < 10:
+	print(x)
+	x += 1
