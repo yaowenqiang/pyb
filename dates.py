e506519f1f4f07021f658496e26d29fbdc301ac7
yaowenqiang <yaowenqiang111@163.com>
2022-02-17 23:44:00 +0800

    add datetime timezone operate demo

--- dates.py
+++ dates.py
@@ -1,4 +1,8 @@
 import datetime
+import pytz
+
+# pip install pytz
+# pip install arrow
 
 d = datetime.date(2015, 7, 20)
 
@@ -49,7 +53,69 @@ print(dt)
 
 print(dt.date())
 print(dt.time())
+print(dt.year)
 
 
 datetimedelta = datetime.timedelta(days=7, hours=4)
 print(dt + datetimedelta)
+
+
+dt_today = datetime.datetime.today()
+dt_now = datetime.datetime.now()
+dt_utcnow = datetime.datetime.utcnow()
+
+print(dt_today)
+print(dt_now)
+print(dt_utcnow)
+
+
+
+
+dt_2 = datetime.datetime(2022,2,27, 11,10, 0, tzinfo=pytz.UTC)
+
+print(dt_2)
+dt_now_2 = datetime.datetime.now(tz=pytz.UTC)
+print(dt_now_2)
+dt_utcnow_2 = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+print(dt_utcnow_2)
+
+
+dt_mtn = dt_utcnow_2.astimezone(pytz.timezone('US/Mountain'))
+print(dt_mtn)
+
+
+
+# for tz in pytz.all_timezones:
+# 	print(tz)
+
+
+
+
+dt_mtn_2 = datetime.datetime.now()
+
+
+print(dt_mtn_2)
+
+
+
+
+mtn_tz = pytz.timezone('US/Mountain')
+
+dt_mtn_2 = mtn_tz.localize(dt_mtn_2)
+print(dt_mtn_2)
+
+dt_east = dt_mtn_2.astimezone(pytz.timezone('US/Eastern'))
+
+print(dt_east)
+
+
+
+
+print(dt_mtn_2.isoformat())
+print(dt_mtn_2.strftime('%B %d, %Y'))
+
+dt_str = 'February 17, 2022'
+
+dt_from_str = datetime.datetime.strptime(dt_str, '%B %d, %Y')
+
+print(dt_from_str)
