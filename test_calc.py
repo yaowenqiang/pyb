de5ee1950b6f163102422951e0928b16c29a1dac
yaowenqiang <yaowenqiang111@163.com>
2022-02-21 23:52:22 +0800

    add unit test demo

---
+++ test_calc.py
@@ -0,0 +1,17 @@
+# python -m unittest test_calc.py
+
+import unittest
+
+import calc
+
+class testCalc(unittest.TestCase):
+
+	def test_add(self):
+		# result = calc.add(10, 5)
+		self.assertEqual(calc.add(10,5), 15)
+		self.assertEqual(calc.add(-1,1), 0)
+		self.assertEqual(calc.add(-1,-1), -2)
+
+
+if __name__ == '__main__':
+	unittest.main()
