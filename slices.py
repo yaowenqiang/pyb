755646b45dc9b0542e7b600ac170115328e2a128
yaowenqiang <yaowenqiang111@163.com>
2022-02-27 23:34:37 +0800

    add slice demo

---
+++ slices.py
@@ -0,0 +1,17 @@
+my_list = list(range(0, 10))
+# print(my_list[-1])
+print(my_list)
+
+print(my_list[3:8])
+print(my_list[-7:-2])
+print(my_list[:len(my_list)])
+print(len(my_list))
+print(my_list[-1])
+print(my_list[::-1])
+
+url = 'https://www.baidu.com'
+
+print(url[::-1])
+
+print(url[-4:])
+print(url[8:])
