8fd69e4e01e33c0d1aa9ea47cbcb0a76f9274585
yaowenqiang <yaowenqiang111@163.com>
2022-03-04 23:32:40 +0800

    add  Forgiveness, not Permission demo

--- duck_typing.py
+++ duck_typing.py
@@ -39,3 +39,43 @@ quack_and_fly(d)
 p = Person()
 quack_and_fly(p)
 
+
+
+
+person = {
+	'name': 'jack',
+	'age': 20,
+	'job':'programmer'
+}
+
+
+
+if 'name' in person and 'age' in person and 'job' in person:
+	print("I'm {name}, I'm {age}, years old and I am a {job}'".format(**person))
+else:
+	person('Missing some keys')
+
+
+try:
+	print("I'm {name}, I'm {age}, years old and I am a {job}'".format(**person))
+except KeyError as e:
+	print('missing {} key'.formate)
+
+
+
+my_list = [1,2,3,4,5]
+
+if len(my_list) > 5:
+	print(my_list[5])
+else:
+	print('That index does not exist')
+
+
+try:
+	print(my_list[5])
+except IndexError as e:
+	print(e)
+
+
+
+
