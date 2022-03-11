fa6fa9152c99728e1907749bc8a02004ff0fcf1f
yaowenqiang <yaowenqiang111@163.com>
2022-03-11 23:42:50 +0800

    add class method  demo

--- oop_class.py
+++ oop_class.py
@@ -5,22 +5,28 @@ class Employee():
 		self.pay = pay
 		self.email = first + '.' + last + '@compay.com'
 
+	def fullname(self):
+		return '{} {}'.format(self.first, self.last)
 
 
 
-emp_1 = Employee()
-emp_2 = Employee()
 
-emp_1.first = 'jack'
-emp_1.last = 'yao'
-emp_1.email = 'jack@yao.com'
-emp_1.pay = 5000
+emp_1 = Employee('jack', 'yao', 2000)
+emp_2 = Employee('text', 'user',4000)
+
+# emp_1.first = 'jack'
+# emp_1.last = 'yao'
+# emp_1.email = 'jack@yao.com'
+# emp_1.pay = 5000
 
 
-emp_2.first = 'text'
-emp_2.last = 'User'
-emp_2.email = 'text@yao.com'
-emp_2.pay = 8000
+# emp_2.first = 'text'
+# emp_2.last = 'User'
+# emp_2.email = 'text@yao.com'
+# emp_2.pay = 8000
 
 print(emp_1)
 print(emp_2)
+print(emp_1.fullname())
+
+print(Employee.fullname(emp_2))
