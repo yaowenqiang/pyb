a9f2894a4f7862648723c076eede15d0a674d70b
yaowenqiang <yaowenqiang111@163.com>
2022-03-13 23:55:44 +0800

    add class method and static method demo '

--- oop_class.py
+++ oop_class.py
@@ -1,3 +1,4 @@
+
 class Employee():
 	raise_amount = 1.04
 	num_of_emps = 0
@@ -16,6 +17,21 @@ class Employee():
 	def apply_raise(self):
 		# self.pay = int(self.pay * Employee.raise_amount)
 		self.pay = int(self.pay * self.raise_amount)
+
+	@classmethod
+	def set_raise_amt(cls, amount):
+		cls.raise_amount = amount
+
+	@classmethod
+	def from_string(cls, emp_str):
+		first ,last, pay = emp_str.split('-')
+		return cls(first, last, pay)
+
+	@staticmethod
+	def is_workday(day):
+		if day.weekday() == 5  or  day.weekday() == 6:
+			return False
+		return True
 
 
 
@@ -62,3 +78,24 @@ print(emp_1.raise_amount)
 print(Employee.raise_amount)
 
 print(Employee.num_of_emps)
+
+
+
+
+Employee.set_raise_amt(2.0)
+# emp_1.set_raise_amt(3.0)
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
+
+emp_str_1 = 'John-doe-5000'
+emp_str_2 = 'Steve-Smith-12000'
+emp_str_3 = 'Jane-Doe-52000'
+
+new_emp_1 = Employee.from_string(emp_str_1)
+print(new_emp_1.email)
+
+import datetime
+
+my_date = datetime.date(2022, 3, 15)
+print(Employee.is_workday(my_date))
