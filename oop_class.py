312063ca4a6ed57d28c90bcb37ab6131c115a824
yaowenqiang <yaowenqiang111@163.com>
2022-03-16 23:48:56 +0800

    add special methods demo

--- oop_class.py
+++ oop_class.py
@@ -33,11 +33,32 @@ class Employee():
 			return False
 		return True
 
+	def __repr__(self):
+		return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
+			
+
+	def __str__(self):
+		return '{} - {}'.format(self.fullname(), self.email)
+
+	def __add__(self, other):
+		return self.pay + other.pay
+
+	def __len__(self,):
+		return len(self.fullname())
 
 
 
 emp_1 = Employee('jack', 'yao', 2000)
+print(emp_1)
+
+print(str(emp_1))
+print(repr(emp_1))
+print(int.__add__(100, 200))
+print(str.__add__('a', 'b'))
 emp_2 = Employee('text', 'user',4000)
+
+print(emp_2 + emp_1)
+print(len(emp_1))
 
 # emp_1.first = 'jack'
 # emp_1.last = 'yao'
