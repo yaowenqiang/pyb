17eeafa56d5043023bfcce80f44fa4824d68d1e7
yaowenqiang <yaowenqiang111@163.com>
2022-03-24 23:57:09 +0800

    add sqlite with class demo

--- sqlite_demo.py
+++ sqlite_demo.py
@@ -1,4 +1,6 @@
 import sqlite3
+from employee import Employee
+
 
 # conn = sqlite3.connect(':memory:')
 conn = sqlite3.connect('employee.db')
@@ -15,5 +17,12 @@ c.execute('select * from employee where last = "yao"')
 print(c.fetchone())
 # c.fetchmany()
 # c.fetchall()
+conn.commit()
+
+
+emp_1 = Employee('john', 'Doe', 8000)
+emp_2 = Employee('jacky', 'yao', 8000)
+
+c.execute('INSERT INTO employee VALUES(:first, :last, :pay)',{'first': emp_1.first, 'last': emp_1.last, 'pay':emp_1.pay})
 conn.commit()
 conn.close()
