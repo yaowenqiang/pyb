a6448d0fac13acd71532d097e1b9237aa2c73d04
yaowenqiang <yaowenqiang111@163.com>
2022-03-27 23:38:36 +0800

    add customer logger demo

--- loggingg_demo.py
+++ loggingg_demo.py
@@ -1,6 +1,15 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG,filename='test.log',format='%(asctime)s:%(levelname)s:%(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('employee.log')
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+# logging.basicConfig(level=logging.DEBUG,filename='test.log',format='%(asctime)s:%(levelname)s:%(message)s')
 def add(x, y):
 	return x + y
 
@@ -11,9 +20,16 @@ def multiply(x, y):
 	return x * y
 
 def divide(x, y):
-	return x / y
+	try:
+		return x / y
+	except ZeroDivisionError:
+		logger.exception('trying to divide by zero')
+	else:
+		return result
 
 num_1 = 10	
 num_2 = 20
 add_result = add(num_1, num_2)
-logging.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+# logging.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+logger.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+divide(1,0)
